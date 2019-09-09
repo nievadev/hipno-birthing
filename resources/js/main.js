4867bcbc99f7d3243cfec443b258f80bc91914a6
Martín Nieva <martinjafactor@gmail.com>
2019-09-09 02:32:49 -0300

    Made the icons into external svg files to make code more readable and less messy, and made close button in collapsible navigation bar to change with every tap onto it

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,17 @@
 var actioned = false;
 
-function actionNav() 
+function actionNav(mobile = false) 
 {
     if (actioned == false) 
     {
         document.getElementById("mySidenav").style.top = "0px";
         document.getElementById("mySidenav").style.boxShadow = "0px 3px 10px #919191";
+
+        if (mobile == true)
+        {
+            document.getElementById("myMenuMobileButton").style.display = "none";
+            document.getElementById("myMenuMobileCloseButton").style.display = "block";
+        }
 
         actioned = true;
     }
@@ -15,6 +21,29 @@ function actionNav()
         document.getElementById("mySidenav").style.top = "-208px";
         document.getElementById("mySidenav").style.boxShadow = "0px 3px 10px transparent";
 
+        if (mobile == true)
+        {
+            document.getElementById("myMenuMobileButton").style.display = "block";
+            document.getElementById("myMenuMobileCloseButton").style.display = "none";
+        }
+
         actioned = false;
     }
 }
+
+function disappear(x)
+{
+    if (x.matches)
+    {
+        document.getElementById("myMobileButton").style.display = "none";
+    }
+
+    else
+    {
+        document.getElementById("myMobileButton").style.display = "block";
+    }
+}
+
+var x = window.matchMedia("(min-width: 533px)");
+disappear(x);
+x.addListener(disappear);
