576d44c25142e313e382834d66e4c958ac778feb
Martín Nieva <martinjafactor@gmail.com>
2019-09-10 21:44:26 -0300

    Modified .gitignore and added more options into the collapsible navigation bar

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 var actioned = false;
+var actioned2 = false;
 
 function actionNav(mobile = false) 
 {
@@ -20,6 +21,7 @@ function actionNav(mobile = false)
     {
         document.getElementById("mySidenav").style.top = "-208px";
         document.getElementById("mySidenav").style.boxShadow = "0px 3px 10px transparent";
+        document.getElementById("myDropdownList").style.height = "0";
 
         if (mobile == true)
         {
@@ -28,6 +30,24 @@ function actionNav(mobile = false)
         }
 
         actioned = false;
+        actioned2 = false;
+    }
+}
+
+function actionDropdown()
+{
+    if (actioned2 == false)
+    {
+        document.getElementById("myDropdownList").style.height = "190px";
+
+        actioned2 = true;
+    }
+
+    else
+    {
+        document.getElementById("myDropdownList").style.height = "0";
+
+        actioned2 = false;
     }
 }
 
@@ -47,3 +67,5 @@ function disappear(x)
 var x = window.matchMedia("(min-width: 533px)");
 disappear(x);
 x.addListener(disappear);
+
+// 382px width
